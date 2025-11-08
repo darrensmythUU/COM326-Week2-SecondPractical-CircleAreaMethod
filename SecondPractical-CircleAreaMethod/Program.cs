e4cdf9204f209ab5e35a66a36ac197ffbab86f83
Kiphea <djsmyth05@gmail.com>
2025-11-08 14:42:14 +0000

    Adding code to different repo to fix name. Task 10+11 complete

--- SecondPractical-CircleAreaMethod/Program.cs
+++ SecondPractical-CircleAreaMethod/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Practical 2
+ * Information: Methods Demo (CircleAreaMethod)
+ * Version 1
+ * Author: Darren Smyth
+ * Date: October 8th 2025
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Please enter the radius of the circle you wish to calculate the area of: ");
+        double radius = CircleArea(Convert.ToDouble(Console.ReadLine()));
+        Console.WriteLine($"The area of the circle is: {radius}.");
+    }
+
+    private static double CircleArea(double radius)
+    {
+        radius = Math.PI * Math.Pow(radius, 2);
+        return radius;
+    }
+}
